toverainc/esp-adf

commit 5d4ac31913e2d912f807fc235796c19315f701eb

audio_recorder: allow setting wake word

components/audio_recorder/recorder_sr.c
@@ -100,6 +100,7 @@ typedef struct __recorder_sr {
     char                  *mn_language;
 #endif /* CONFIG_USE_MULTINET */
     int8_t                input_order[DAT_CH_MAX];
+    char                  *wn_wakeword;
 } recorder_sr_t;
 
 static esp_err_t recorder_sr_output(recorder_sr_t *recorder_sr, void *buffer, int len);
@@ -524,6 +525,7 @@ recorder_sr_handle_t recorder_sr_create(recorder_sr_cfg_t *cfg, recorder_sr_ifac
     recorder_sr->rb_size          = cfg->rb_size;
     recorder_sr->partition_label  = cfg->partition_label;
     recorder_sr->aec_enable       = cfg->afe_cfg.aec_init;
+    recorder_sr->wn_wakeword      = cfg->wn_wakeword;
 #ifdef CONFIG_USE_MULTINET
     recorder_sr->mn_language      = cfg->mn_language;
 #endif
@@ -531,7 +533,7 @@ recorder_sr_handle_t recorder_sr_create(recorder_sr_cfg_t *cfg, recorder_sr_ifac
     memcpy(recorder_sr->input_order, cfg->input_order, DAT_CH_MAX);
 
     recorder_sr->models = esp_srmodel_init(recorder_sr->partition_label);
-    char *wn_name = esp_srmodel_filter(recorder_sr->models, ESP_WN_PREFIX, NULL);
+    char *wn_name = esp_srmodel_filter(recorder_sr->models, ESP_WN_PREFIX, recorder_sr->wn_wakeword);
     AUDIO_NULL_CHECK(TAG, wn_name, goto _failed);
     cfg->afe_cfg.wakenet_model_name = wn_name;
     recorder_sr->afe_handle = esp_afe->create_from_config(&cfg->afe_cfg);

components/audio_recorder/include/recorder_sr.h
@@ -64,6 +64,7 @@ typedef struct {
     int          rb_size;                               /*!< Ringbuffer size of recorder sr */
     char         *partition_label;                      /*!< Partition label which stored the model data */
     char         *mn_language;                          /*!< Command language for multinet to load */
+    char         *wn_wakeword;                          /*!< Wake Word for WakeNet to load. This is useful when multiple Wake Words are selected in sdkconfig. Setting this to NULL will use the first found model. */
 } recorder_sr_cfg_t;
 
 #if CONFIG_AFE_MIC_NUM == (1)
@@ -99,6 +100,7 @@ typedef struct {
     .rb_size          = SR_OUTPUT_RB_SIZE,      \
     .partition_label  = "model",                \
     .mn_language      = ESP_MN_CHINESE,         \
+    .wn_wakeword      = NULL,                   \
 };
 
 /**

commit cc1d375e149d2239ba7bef84de43b27c3a4b669a

audio_recorder: restore ability to select wake word
Commit 5d4ac31913e2 ("audio_recorder: allow setting wake word")
introduced the ability to select which wake word to use at runtime.
This feature was dropped in a later commit. Restore it.

Fixes: 3f6abbb15b85 ("audio_recorder: support new feature of esp-sr (break change of `audio_recorder`)")

components/audio_recorder/recorder_sr.c
@@ -563,21 +563,25 @@ recorder_sr_handle_t recorder_sr_create(recorder_sr_cfg_t *cfg, recorder_sr_ifac
     char *wn_name = NULL;
     char *wn_name_2 = NULL;
 
-    if (recorder_sr->models!=NULL) {
-        for (int i = 0; i< recorder_sr->models->num; i++) {
-            if (strstr(recorder_sr->models->model_name[i], ESP_WN_PREFIX) != NULL) {
-                if (wn_name == NULL) {
-                    wn_name = recorder_sr->models->model_name[i];
-                    ESP_LOGI(TAG, "The first wakenet model: %s\n", wn_name);
-                } else if (wn_name_2 == NULL) {
-                    wn_name_2 = recorder_sr->models->model_name[i];
-                    ESP_LOGI(TAG, "The second wakenet model: %s\n", wn_name_2);
+    if (recorder_sr->wn_wakeword == NULL) {
+        if (recorder_sr->models!=NULL) {
+            for (int i = 0; i< recorder_sr->models->num; i++) {
+                if (strstr(recorder_sr->models->model_name[i], ESP_WN_PREFIX) != NULL) {
+                    if (wn_name == NULL) {
+                        wn_name = recorder_sr->models->model_name[i];
+                        ESP_LOGI(TAG, "The first wakenet model: %s\n", wn_name);
+                    } else if (wn_name_2 == NULL) {
+                        wn_name_2 = recorder_sr->models->model_name[i];
+                        ESP_LOGI(TAG, "The second wakenet model: %s\n", wn_name_2);
+                    }
                 }
             }
+        } else {
+            ESP_LOGE(TAG, "Please enable wakenet model and select wake word by menuconfig!\n");
+            goto _failed;
         }
     } else {
-        ESP_LOGE(TAG, "Please enable wakenet model and select wake word by menuconfig!\n");
-        goto _failed;
+        wn_name = esp_srmodel_filter(recorder_sr->models, ESP_WN_PREFIX, recorder_sr->wn_wakeword);
     }
 
     cfg->afe_cfg.wakenet_model_name = wn_name;